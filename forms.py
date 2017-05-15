d589b73dd43d124419e61830aa0c67326418d61e
J.R. Leeman <kd5wxb@gmail.com>
2017-05-15 08:48:54 -0600

    Add capability to add and delete feature collections

--- forms.py
+++ forms.py
@@ -3,9 +3,13 @@ from wtforms import SubmitField, TextField, TextAreaField, validators, Form
 
 class CreateDatasetForm(Form):
     name = TextField('name', validators=[validators.DataRequired()])
-
     path = TextField('path', validators=[validators.DataRequired()])
-
     metadata = TextAreaField('metadata', validators=[validators.DataRequired()])
-
     submit = SubmitField('submit')
+
+class CreateFeatureCollectionForm(Form):
+        name = TextField('name', validators=[validators.DataRequired()])
+        path = TextField('path', validators=[validators.DataRequired()])
+        xml = TextAreaField('xml', validators=[validators.DataRequired()])
+        pqact = TextAreaField('pqact', validators=[validators.DataRequired()])
+        submit = SubmitField('submit')
--- tdscreator.py
+++ tdscreator.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, url_for, request
 import json
 
-from forms import CreateDatasetForm
+from forms import CreateDatasetForm, CreateFeatureCollectionForm
 
 app = Flask(__name__)
 app.secret_key = 'NTOBiFxcjaehKa9nvgTmv5dslPUay7l4QDauEGIV3pSwpZKhpFGqJzestVyGODNT7BL8mauL38xyzgukYV3cIMix9eO8Jgb3bhvo'
@@ -53,7 +53,39 @@ def dataset_deletedataset():
 
 @app.route('/featurecollections')
 def featurecollections_view():
-    return render_template('viewfeaturecollections.html')
+    jsondata = open('featurecollections.json', 'r').read()
+    featurecollections = json.loads(jsondata)
+    return render_template('viewfeaturecollections.html', featurecollections=featurecollections)
+
+
+@app.route('/featurecollections/createfeaturecollection', methods=['GET','POST'])
+def featurecollections_createfeaturecollection():
+    form = CreateFeatureCollectionForm(request.form)
+    if request.method=='POST' and form.validate():
+        name = form.name.data
+        xml = form.xml.data
+        jsondata = open('featurecollections.json', 'r').read()
+        datasets = json.loads(jsondata)
+        datasets.append({"name":name, "xml":xml})
+        with open('featurecollections.json', 'w') as outfile:
+            json.dump(datasets, outfile)
+        return redirect(url_for('featurecollections_view'))
+    return render_template('createfeaturecollection.html', createfeaturecollectionform=form)
+
+
+@app.route('/featurecollections/deletefeaturecollection')
+def featurecollections_deletefeaturecollection():
+    featurecollection_name = request.args.get("featurecollection_name")
+    jsondata = open('featurecollections.json', 'r').read()
+    featurecollections = json.loads(jsondata)
+    for i, featurecollection in enumerate(featurecollections):
+        if featurecollection["name"] == featurecollection_name:
+            del featurecollections[i]
+            break
+
+    with open('featurecollections.json', 'w') as outfile:
+        json.dump(featurecollections, outfile)
+    return redirect(url_for('featurecollections_view'))
 
 
 if __name__ == "__main__":
