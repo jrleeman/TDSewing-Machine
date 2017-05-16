8ef20897ac31675c1e177afc3273a2b505e33128
J.R. Leeman <kd5wxb@gmail.com>
2017-05-16 07:35:58 -0600

    Dress up pages with horizontal tables

--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 #from flask_wtf import Form
-from wtforms import SubmitField, TextField, TextAreaField, validators, Form
+from wtforms import SubmitField, TextField, TextAreaField, validators, Form, HiddenField, SelectField
 
 class CreateDatasetForm(Form):
     name = TextField('name', validators=[validators.DataRequired()])
-    path = TextField('path', validators=[validators.DataRequired()])
+    path = SelectField('path', choices=[('t1', 'Test 1'), ('t2', 'Test 2')], validators=[validators.DataRequired()])
+    xml = TextAreaField('xml', validators=[validators.DataRequired()])
     metadata = TextAreaField('metadata', validators=[validators.DataRequired()])
     submit = SubmitField('submit')
 
 class CreateFeatureCollectionForm(Form):
-        name = TextField('name', validators=[validators.DataRequired()])
-        path = TextField('path', validators=[validators.DataRequired()])
-        xml = TextAreaField('xml', validators=[validators.DataRequired()])
-        pqact = TextAreaField('pqact', validators=[validators.DataRequired()])
-        submit = SubmitField('submit')
+    name = TextField('name', validators=[validators.DataRequired()])
+    dataset = SelectField('dataset', choices=[('t1', 'Test 1'), ('t2', 'Test 2')], validators=[validators.DataRequired()])
+    xml = TextAreaField('xml', validators=[validators.DataRequired()])
+    pqact = TextAreaField('pqact', validators=[validators.DataRequired()])
+    submit = SubmitField('submit')
