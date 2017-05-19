72a05b705f8b630a0ed4f8ac72cade5e1aa78fef
J.R. Leeman <kd5wxb@gmail.com>
2017-05-19 14:17:23 -0600

    Style cleanup

--- datasets.py
+++ datasets.py
@@ -1,10 +1,11 @@
+"""Contains a simple dataset storage and manipulation set of tools."""
+
 import hashlib
 import json
 
 
-def get_ordered(dataset_id, ordered, fname='datasets.json'):
-    """Return an ordered list of datasets"""
-    datasets = read_datasets()
+def get_ordered(dataset_id, ordered):
+    """Return an ordered list of datasets."""
     dataset = get_dataset_by_id(dataset_id)
     ordered.append(dataset)
     for child in get_children(dataset_id):
@@ -12,12 +13,10 @@ def get_ordered(dataset_id, ordered, fname='datasets.json'):
     return ordered
 
 
-def get_depth(dataset_id, fname='datasets.json'):
-    """Get depth of any dataset, with 0 being the root"""
-    datasets = read_datasets()
+def get_depth(dataset_id):
+    """Get depth of any dataset, with 0 being the root."""
     dataset = get_dataset_by_id(dataset_id)
-
-    level =0
+    level = 0
     while dataset['parent_dataset'] != 'None':
         dataset = get_dataset_by_id(dataset['parent_dataset'])
         level += 1
@@ -25,7 +24,7 @@ def get_depth(dataset_id, fname='datasets.json'):
 
 
 def get_children(dataset_id, fname='datasets.json'):
-    """Get any immediate children of the given dataset"""
+    """Get any immediate children of the given dataset."""
     datasets = read_datasets()
     children = []
     for ds in datasets:
@@ -48,7 +47,7 @@ def _write_datasets(datasets, fname='datasets.json'):
 
 
 def _rebuild_children_parent_id(old_parent_dataset_id, new_parent_dataset_id):
-    """If any datasets have this parent, update their parent hash"""
+    """If any datasets have this parent, update their parent hash."""
     datasets = read_datasets()
     for ds in datasets:
         if ds['parent_dataset'] == old_parent_dataset_id:
@@ -59,13 +58,14 @@ def _rebuild_children_parent_id(old_parent_dataset_id, new_parent_dataset_id):
 def _create_dataset_id(dataset):
     """Create a unique dataset id by hashing the dataset minus the id."""
     dataset.pop('_id', None)
-    dataset_id = hashlib.sha1(json.dumps(dataset, sort_keys=True).encode('utf-8')).hexdigest()
+    dataset_id = hashlib.sha1(json.dumps(dataset, sort_keys=True).
+                              encode('utf-8')).hexdigest()
     dataset['_id'] = dataset_id
     return dataset
 
 
 def add_dataset(dataset):
-    """Add a new dataset"""
+    """Add a new dataset."""
     datasets = read_datasets()
     dataset = _create_dataset_id(dataset)
     datasets.append(dataset)
@@ -89,25 +89,25 @@ def delete_dataset(dataset_id, delete_below=True):
 
 
 def edit_dataset(dataset_id, dataset):
-    """Given the old dataset id and new data, update the record and children"""
+    """Edit a dataset and rebuild any children."""
     old_dataset_id = dataset_id
     delete_dataset(dataset_id)
     new_dataset_id = add_dataset(dataset)
     _rebuild_children_parent_id(old_dataset_id, new_dataset_id)
 
 
-def get_dataset_by_id(id):
-    """Return a dataset with the given ID"""
+def get_dataset_by_id(dataset_id):
+    """Return a dataset with the given ID."""
     datasets = read_datasets()
     for dataset in datasets:
-        if dataset['_id'] == id:
+        if dataset['_id'] == dataset_id:
             return dataset
 
 
 def get_dataset_by_name(name):
-    """Returns dataset(s) with the given name"""
+    """Retreive dataset by given name."""
     datasets = read_datasets()
-    matching_datasets=[]
+    matching_datasets = []
     for dataset in datasets:
         if dataset['name'] == name:
             matching_datasets.append(dataset)
