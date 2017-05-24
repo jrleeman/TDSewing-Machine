0fb226772bf4bffd1e7d71271a0c6ce5608f1488
J.R. Leeman <kd5wxb@gmail.com>
2017-05-24 07:59:15 -0600

    Toy example showing dataset layout

--- tdscreator.py
+++ tdscreator.py
@@ -26,203 +26,53 @@ def home():
 #
 # Dataset routes
 #
+
+def find_child_datasets(root):
+    ns = {'tdscat': 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0'}
+    return root.findall('./tdscat:dataset', ns)
+
+
+def find_child_dataset_scans(root):
+    ns = {'tdscat': 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0'}
+    return root.findall('./tdscat:datasetScan', ns)
+
+
+def find_child_feature_collections(root):
+    ns = {'tdscat': 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0'}
+    return root.findall('./tdscat:featureCollection', ns)
+
+
+def make_tree(root, tree={}):
+    ns = {'tdscat': 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0'}
+    for dataset in root.findall('./tdscat:dataset', ns):
+        child_datasets = find_child_datasets(dataset)
+        local = {}
+        for child in child_datasets:
+            local[child.attrib['name']] = make_tree(dataset, tree={})
+
+        for child in find_child_feature_collections(dataset):
+            local[child.attrib['name']] = {}
+
+        for child in find_child_dataset_scans(dataset):
+            local[child.attrib['name']] = {}
+
+        tree[dataset.attrib['name']] = local
+    return tree
+
 @app.route('/datasets')
 def dataset_view():
     """Show datasets."""
-    datasets = ds.read_datasets()
-    root = ds.get_dataset_by_name('root')
-    datasets = ds.get_ordered(root['_id'], ordered=[])
+    import xml.etree.ElementTree as ET
 
-    # Get the depth or level of all datasets
-    depths = []
-    for dataset in datasets:
-        depth = ds.get_depth(dataset['_id'])
-        dataset['depth'] = depth
-        depths.append(depth)
+    # Read the tree and send to template as a dict of dicts
+    tree = ET.parse('forecastProdsAndAna.xml')
+    root = tree.getroot()
+    #datasets = make_dictionary_tree(root)
+    #datasets = {'a': {'b': {'c': {}}}}
+    #datasets = {'1': {'1.1': {'1.1.1':{}}}, '2':{}})
+    datasets = make_tree(root, {})
 
-    if len(datasets) > 0:
-        maxdepth = max(depths)
-    else:
-        maxdepth = 0
-    return render_template('viewdatasets.html', datasets=datasets,
-                           maxdepth=maxdepth)
-
-
-@app.route('/datasets/createdataset', methods=['GET', 'POST'])
-def dataset_createdataset():
-    """Create a new dataset."""
-    form = CreateDatasetForm(request.form)
-    # Populate the possible routes for the datasets
-    datasets = ds.read_datasets()
-    form.path.choices = populate_choices(datasets)
-
-    # If a valid form is submitted, create the dataset and add it
-    # TODO: Validate with form.validate():
-    if request.method == 'POST':
-        # Grab info from the form
-        name = form.name.data
-        path = form.path.data
-        metadata = form.metadata.data
-
-        # Create new dataset dictionary and add it to the json file
-        new_dataset = {'name': name, 'parent_dataset': path,
-                       'metadata': metadata, 'feature_collections': []}
-        ds.add_dataset(new_dataset)
-
-        # After sucessful dataset creation, redirect to the dataset view page
-        return redirect(url_for('dataset_view'))
-    # Render the page
-    return render_template('createdataset.html', createdatasetform=form)
-
-
-@app.route('/datasets/editdataset', methods=['GET', 'POST'])
-def dataset_editdataset():
-    """Edit an existing dataset."""
-    form = CreateDatasetForm(request.form)
-    # Populate the possible routes for the datasets
-    datasets = ds.read_datasets()
-    form.path.choices = populate_choices(datasets)
-
-    # If a valid form is submitted, create the dataset and add it
-    if request.method == 'POST':  # and form.validate():
-        if request.form['submit'] == 'Save':
-            # Grab info from the form
-            name = form.name.data
-            path = form.path.data
-            metadata = form.metadata.data
-            old_dataset_id = form.id.data
-
-            # Create new dataset dictionary and add it to the json file
-            new_dataset = {'name': name, 'parent_dataset': path,
-                           'metadata': metadata, 'feature_collections': []}
-            ds.edit_dataset(old_dataset_id, new_dataset)
-        if request.form['submit'] == 'Delete':
-            ds.delete_dataset(form.id.data)
-
-        # After sucessful dataset creation, redirect to the dataset view page
-        return redirect(url_for('dataset_view'))
-
-    # Otherwise, pre-populate the form with data on the dataset
-    else:
-        # Create the form and select the parent dataset of the dataset we
-        # are editing
-        dataset_id = request.args.get('_id')
-        dataset = ds.get_dataset_by_id(dataset_id)
-        form = CreateDatasetForm(request.form, path=dataset['parent_dataset'])
-
-        # Populate the possible routes for the datasets
-        datasets = ds.read_datasets()
-        choices = populate_choices(datasets, exclude=[dataset_id])
-        form.path.choices = choices
-
-        # Get the dataset and pre-populate the form
-        dataset = ds.get_dataset_by_id(dataset_id)
-
-        form.name.data = dataset['name']
-        form.metadata.data = dataset['metadata']
-        form.id.data = dataset['_id']
-
-    # Render the page
-    return render_template('editdataset.html', createdatasetform=form,
-                           dataset=dataset)
-
-
-@app.route('/datasets/deletedataset')
-def dataset_deletedataset():
-    """Remove a dataset."""
-    ds.deletedataset(request.args.get('dataset_id'))
-    return redirect(url_for('dataset_view'))
-
-
-#
-# Feature Collection routes
-#
-
-@app.route('/featurecollections')
-def featurecollections_view():
-    """View feature collections."""
-    return render_template('notready.html')
-    jsondata = open('featurecollections.json', 'r').read()
-    featurecollections = json.loads(jsondata)
-    return render_template('viewfeaturecollections.html',
-                           featurecollections=featurecollections)
-
-
-@app.route('/featurecollections/createfeaturecollection',
-           methods=['GET', 'POST'])
-def featurecollections_createfeaturecollection():
-    """Create new feature collection."""
-    form = CreateFeatureCollectionForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        xml = form.xml.data
-        jsondata = open('featurecollections.json', 'r').read()
-        datasets = json.loads(jsondata)
-        datasets.append({'name': name, 'xml': xml})
-        with open('featurecollections.json', 'w') as outfile:
-            json.dump(datasets, outfile)
-        return redirect(url_for('featurecollections_view'))
-    return render_template('createfeaturecollection.html',
-                           createfeaturecollectionform=form)
-
-
-@app.route('/featurecollections/deletefeaturecollection')
-def featurecollections_deletefeaturecollection():
-    """Remove a feature collection."""
-    featurecollection_name = request.args.get('featurecollection_name')
-    jsondata = open('featurecollections.json', 'r').read()
-    featurecollections = json.loads(jsondata)
-    for i, featurecollection in enumerate(featurecollections):
-        if featurecollection['name'] == featurecollection_name:
-            del featurecollections[i]
-            break
-
-    with open('featurecollections.json', 'w') as outfile:
-        json.dump(featurecollections, outfile)
-    return redirect(url_for('featurecollections_view'))
-
-
-#
-# My Servers
-#
-
-
-@app.route('/servers')
-def servers():
-    """View servers in user's account."""
-    return render_template('notready.html')
-
-
-#
-# Helpers
-#
-
-
-def get_path(datasets, dataset):
-    """Get path for dataset."""
-    path = []
-    dst = dataset
-    while dst['parent_dataset'] != 'None':
-        path.append(dst['name'])
-        dst = ds.get_dataset_by_id(dst['parent_dataset'])
-    path_str = ''
-    for item in path[::-1]:
-        path_str += '/' + item
-    return path_str
-
-
-def populate_choices(datasets, exclude=[]):
-    """Popuate a dropdown with choices."""
-    choices = []
-    paths = []
-    for dst in datasets:
-        if dst['_id'] not in exclude:
-            paths.append(get_path(datasets, dst))
-            name = paths[-1]
-            if name == '':
-                name = '/'
-            choices.append((dst['_id'], name))
-    return choices
+    return render_template('viewdatasets.html', datasets=datasets)
 
 
 if __name__ == '__main__':
